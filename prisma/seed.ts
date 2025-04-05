d919a2906ae8f8bdf05bdd17ffc8a0ad760dff44
Kocsis Bars Nimród <kocsisb0401@gmail.com>
2025-04-05 13:40:30 +0200

    Admin hozzáadása seed.ts-el

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import * as argon2 from 'argon2';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const adminUsername = 'admin';
+  const adminExists = await prisma.user.findUnique({
+    where: { username: adminUsername },
+  });
+
+  if (!adminExists) {
+    const hashedPassword = await argon2.hash('Adminpass123!'); 
+
+    await prisma.user.create({
+      data: {
+        username: adminUsername,
+        password: hashedPassword,
+        admin: 'ADMIN',
+      },
+    });
+    console.log('Default admin user created.');
+  } else {
+    console.log('Admin user already exists.');
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
