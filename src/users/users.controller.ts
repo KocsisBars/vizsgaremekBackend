b268ae97b79b9dd38d53e5356a68704e83bd46b8
Kocsis Bars Nimród <kocsisb0401@gmail.com>
2025-04-16 19:46:42 +0200

    Swagger api kitöltése

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,12 +3,27 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiNotFoundResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({
+    status: 201,
+    description: 'User created successfully',
+    schema: {
+      example: {
+        id: 1,
+        username: 'Proba1',
+        role: 'USER',
+        points: 0,
+      },
+    },
+  })
+  @ApiBadRequestResponse({ description: 'This username already exists' })
   async create(@Body() createUserDto: CreateUserDto) {
     try {
       return await this.usersService.create(createUserDto);
@@ -20,6 +35,27 @@ export class UsersController {
 
   @Post('add-points')
   @UseGuards(AuthGuard('bearer'))
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Add points to the authenticated user's account" })
+  @ApiBody({
+    schema: {
+      example: {
+        pointsToAdd: 20,
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Points successfully added',
+    schema: {
+      example: {
+        id: 1,
+        username: 'Proba1',
+        points: 100,
+        role: 'USER',
+      },
+    },
+  })
   async addPoints(@Body() body: { pointsToAdd: number }, @Req() req: any) {
     const userId = req.user.id; 
     return this.usersService.addPoints(userId, body.pointsToAdd);
@@ -27,11 +63,41 @@ export class UsersController {
 
   @Get()
   @UseGuards(AuthGuard('bearer'))
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of all users',
+    schema: {
+      example: [
+        {
+          id: 1,
+          username: 'Proba1',
+          points: 30,
+          role: 'USER',
+        },
+      ],
+    },
+  })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a user by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'User found',
+    schema: {
+      example: {
+        id: 1,
+        username: 'Proba1',
+        points: 30,
+        role: 'USER',
+      },
+    },
+  })
+  @ApiNotFoundResponse({ description: 'User with id not found' })
   async findOne(@Param('id') id: string) {
     const user = await this.usersService.findOne(+id);
     if (!user) {
@@ -41,6 +107,9 @@ export class UsersController {
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a user by ID' })
+  @ApiResponse({ status: 200, description: 'User updated successfully' })
+  @ApiNotFoundResponse({ description: 'User with id not found' })
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     const user = await this.usersService.findOne(+id);
     if (!user) {
@@ -50,6 +119,8 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a user by ID' })
+  @ApiResponse({ status: 200, description: 'User deleted successfully' })
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
